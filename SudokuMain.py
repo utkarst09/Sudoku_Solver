bea36625ef65e4d886bc10ea01fe61f00eeea883
Utkarsh Tiwari <tiwari_831937@student.nitw.ac.in>
2022-08-10 18:25:17 +0530

    Finished

--- SudokuMain.py
+++ SudokuMain.py
@@ -5,7 +5,7 @@ from utlis import *
 import Solver
 
 ########################################################################
-pathImage = "Resources/3.jpg"
+pathImage = "Resources/8.png"
 heightImg = 450
 widthImg = 450
 model = intializePredectionModel()  # LOAD THE CNN MODEL
@@ -26,10 +26,10 @@ cv2.drawContours(imgContours, contours, -1, (0, 255, 0), 3) # DRAW ALL DETECTED 
 
 #### 3. FIND THE BIGGEST COUNTOUR AND USE IT AS SUDOKU
 biggest, maxArea = biggestContour(contours) # FIND THE BIGGEST CONTOUR
-print(biggest)
+# print(biggest)
 if biggest.size != 0:
     biggest = reorder(biggest)
-    print(biggest)
+    # print(biggest)
     cv2.drawContours(imgBigContour, biggest, -1, (0, 0, 255), 25) # DRAW THE BIGGEST CONTOUR
     pts1 = np.float32(biggest) # PREPARE POINTS FOR WARP
     pts2 = np.float32([[0, 0],[widthImg, 0], [0, heightImg],[widthImg, heightImg]]) # PREPARE POINTS FOR WARP
@@ -41,24 +41,30 @@ if biggest.size != 0:
     #### 4. SPLIT THE IMAGE AND FIND EACH DIGIT AVAILABLE
     imgSolvedDigits = imgBlank.copy()
     boxes = splitBoxes(imgWarpColored)
-    print(len(boxes))
-    cv2.imshow("Sample",boxes[2])
+    # print(len(boxes))
+    # cv2.imshow("Sample",boxes[2])
     numbers = getPredection(boxes, model)
-    print(numbers)
+    # print(numbers)
     imgDetectedDigits = displayNumbers(imgDetectedDigits, numbers, color=(255, 0, 255))
     numbers = np.asarray(numbers)
     posArray = np.where(numbers > 0, 0, 1)
-    print(posArray)
+    # print(posArray)
 
 
     #### 5. FIND SOLUTION OF THE BOARD
     board = np.array_split(numbers,9)
-    print(board)
+    print()
+    print('******* Input Sudoko *******')
+    print()
+    Solver.print_board(board)
     try:
         Solver.solve(board)
     except:
         pass
-    print(board)
+    print()
+    print('********** Answer **********')
+    print()
+    Solver.print_board(board)
     flatList = []
     for sublist in board:
         for item in sublist:
